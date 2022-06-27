990cf3f5b353c5b07b9f70cae286af0a51fa2d55
SpeedyGonzale5 <peternevarez2@gmail.com>
2022-06-27 17:27:01 -0400

    modified redeploy script to use service instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tmux kill-server && cd mlhportfolio && git fetch && git reset origin/main –hard 01~ && source python3-virtualenv/bin/activate && pip3 install -r requirements.txt && tmux && flask run –host=0.0.0.0
+cd mlhportfolio && git fetch && git reset origin/main -hard  && source python3-virtualenv/bin/activate && pip3 install -r && requirements.txt && systemctl restart mlhportfolio && flask run --host=0.0.0.0
