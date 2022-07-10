a8fc080b0d2469ca5d748431c50f9c6993e39c46
SpeedyGonzale5 <peternevarez2@gmail.com>
2022-07-10 18:06:02 -0400

    modified redeploy script for docker container and added text text in index.html

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd mlhportfolio && git fetch && git reset origin/main -hard  && source python3-virtualenv/bin/activate && pip3 install -r && requirements.txt && systemctl restart mlhportfolio && flask run --host=0.0.0.0
+cd mlhportfolio && git fetch && git reset origin/main -hard  && docker compose -f docker-compose.prod.yml down && docker compose -f docker-compose.prod.yml up -d --build
