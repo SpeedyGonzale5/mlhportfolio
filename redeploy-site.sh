0441f0b7ccfad3c4f85dd48a537b667da3fae821
SpeedyGonzale5 <peternevarez2@gmail.com>
2022-07-10 18:24:58 -0400

    modified redeploy-side.sh for docker containers

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd mlhportfolio && git fetch && git reset origin/main -hard  && docker compose -f docker-compose.prod.yml down && docker compose -f docker-compose.prod.yml up -d --build
+cd mlhportfolio && git fetch && git reset origin/main --hard  && docker compose -f docker-compose.prod.yml down && docker compose -f docker-compose.prod.yml up -d --build
