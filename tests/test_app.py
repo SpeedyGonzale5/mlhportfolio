87d7ed20ccc7986cc449ddb904badfce8a6efb09
SpeedyGonzale5 <peternevarez2@gmail.com>
2022-07-14 14:27:10 -0400

    added This is our test to the first paragraph text on homepage

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,14 @@
+from app import app
 import unittest
 import os
 import json
 os.environ['TESTING'] = 'true'
 
-from app import app
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
-    
+
     def test_home(self):
         response = self.client.get("/")
         assert response.status_code == 200
@@ -20,7 +20,7 @@ class AppTestCase(unittest.TestCase):
         assert "Scroll through our website and witness what we've accomplished!" in html
         assert "Cras dui velit" in html
         self.assertIsNotNone(html)
-    
+
     def test_timeline(self):
         response = self.client.get("/api/timeline_post")
         assert response.status_code == 200
@@ -30,7 +30,8 @@ class AppTestCase(unittest.TestCase):
         assert len(json["timeline_posts"]) == 0
 
         # Tests relating to GET and POST endpoints
-        post = self.client.post("/api/timeline_post", data=dict({"name": "John Doe", "email": "john@example.com", "content": "Hello world, I\'m John!"}))
+        post = self.client.post("/api/timeline_post", data=dict(
+            {"name": "John Doe", "email": "john@example.com", "content": "Hello world, I\'m John!"}))
         assert post.status_code == 200
 
         response_2 = self.client.get("/api/timeline_post")
@@ -44,7 +45,7 @@ class AppTestCase(unittest.TestCase):
         response_3 = self.client.get("/timeline2")
         assert response_3.status_code == 200
         html = response_3.get_data(as_text=True)
-        assert "<input name=\"name\" type=\"text\"/>" in html
+        assert "<input name=\"name\" type=\"text\" />" in html
         assert "<input name=\"email\" type=\"text\" />" in html
         assert "<input name=\"content\" type=\"text\" />" in html
         assert "<button type=\"submit\">Submit</button>" in html
@@ -52,24 +53,23 @@ class AppTestCase(unittest.TestCase):
         self.assertIsNotNone(html)
 
     def test_malformed_timeline_post(self):
-        #POST request missing name
-        response = self.client.post("/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
+        # POST request missing name
+        response = self.client.post(
+            "/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
         assert response.status_code == 400
         html = response.get_data(as_text=True)
         assert "Invalid name" in html
 
-        #POST request with empty content
-        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": ""})
+        # POST request with empty content
+        response = self.client.post(
+            "/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": ""})
         assert response.status_code == 400
         html = response.get_data(as_text=True)
         assert "Invalid content" in html
 
-        #POST request with malformed email
-        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
+        # POST request with malformed email
+        response = self.client.post("/api/timeline_post", data={
+                                    "name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
         assert response.status_code == 400
         html = response.get_data(as_text=True)
         assert "Invalid email" in html
-
-
-
-
