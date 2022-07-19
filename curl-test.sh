34d0de4cdb28602adae8cdb96bac52e804015e70
SpeedyGonzale5 <peternevarez2@gmail.com>
2022-07-18 20:41:25 -0400

    Modified run_test.sh to work

--- curl-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -request POST http://104.248.14.212:5000/api/timeline_post -d 'name=Weib&email=wei.he@mlh.io&content=Just Added Database to my portfolio site!' && curl --request GET http://104.248.14.212:5000/api/timeline_post | jq '.'
